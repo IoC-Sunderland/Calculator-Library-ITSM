9568211f7334967baa601dfeb64cff988009821a
Institute of Coding (Sunderland) <gavin.mcclary@sunderland.ac.uk>
2020-11-03 14:32:38 +0000

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,6 @@ class TestCalculator:
 
     def test_addition(self):
         assert 10 == calculator_functions.addition(5, 5)
+
+    def test_subtract(self):
+        assert 5 == calculator_functions.subtract(10, 5)
